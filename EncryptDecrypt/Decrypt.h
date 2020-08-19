a664d926b975576f63f9f0f2834857f8a2d60d0c
wai1 <wai1@cornell.edu>
2020-08-19 16:56:01 -0400

    Update Decrypt.h
    Added additional comments

--- EncryptDecrypt/Decrypt.h
+++ EncryptDecrypt/Decrypt.h
@@ -9,19 +9,18 @@ public:
 	~Decrypt();  // Destructor
 
 	std::string dCryptString(std::string encryptedString);
-
-	// An iterator for string which will not change the value of the string
-	std::string::const_iterator decryptIt{};
+	
+	std::string::const_iterator decryptIt{};  // An iterator for string which will not change the value of the string
 
 private:
-	void theDecoder(std::string temp, bool lastIteration); 
-	bool CheckForNumber(std::string encodedChar);
+	void theDecoder(std::string temp, bool lastIteration); // Function that decodes
+	bool CheckForNumber(std::string encodedChar);  // Function tests for numbers
 
 	bool aNumber = false;  // For return of function to test for numbers
 	int duplicates = 0;  // Keeps track of number of duplicates
 	int tempDuplicate = 0;  // For storing new number read in
 	std::string decoded{};  // Contains the encoded string
 	std::string encodedChar{};  // Holds string character read in
-	std::string decryptedString{};
+	std::string decryptedString{};  // Contains decrypted string
 };
 
